4e26f694ed28ea8f8a72177d1644942666cf96d1
Rehan Pinjari <pinjarirehan11@gmail.com>
2024-10-03 17:02:18 +0530

    Added: Transform Each Element To Double Using Map Function

--- src/com/bridglabz/java/streamsapi/StreamExample.java
+++ src/com/bridglabz/java/streamsapi/StreamExample.java
@@ -2,6 +2,7 @@ package com.bridglabz.java.streamsapi;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamExample {
 
@@ -10,8 +11,12 @@ public class StreamExample {
         //Create a list of integers
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
 
-        // Create a stream from the list and use forEach to iterate over the elements
-        numbers.stream().forEach(number -> System.out.println("Number " + number));
+        // Use stream.map() to transform each element to double and store the result
+        List<Double> doubleNumbers = numbers.stream().map(number -> number * 2.0) //Transform each element to double
+                                                     .collect(Collectors.toList()); // Collect the result into a new list
+
+        //Print the transformed list
+        doubleNumbers.forEach(doubleNumber -> System.out.println("Double Number " + doubleNumber));
 
 
     }
