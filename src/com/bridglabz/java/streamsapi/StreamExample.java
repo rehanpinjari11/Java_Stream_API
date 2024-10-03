23ae65c5a77c2e4d1655e90fb7943b0d2ee00cbf
Rehan Pinjari <pinjarirehan11@gmail.com>
2024-10-03 17:47:04 +0530

    Added: Show First Even Number In Number Stream Using findFirst Function

--- src/com/bridglabz/java/streamsapi/StreamExample.java
+++ src/com/bridglabz/java/streamsapi/StreamExample.java
@@ -2,21 +2,21 @@ package com.bridglabz.java.streamsapi;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 public class StreamExample {
 
     public static void main(String[] args)
     {
         //Create a list of integers
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> numbers = Arrays.asList(1, 3, 5, 6, 7, 8, 10, 11, 12);
 
-        // Use stream.map() to transform each element to double and store the result
-        List<Integer> evenNumbers = numbers.stream().filter(number -> number % 2 == 0) // Filter even numbers
-                                                     .collect(Collectors.toList()); // Collect the filtered numbers into a new list
+        // Use stream.filter() to find even numbers, and findFirst() to get the first even number
+        Optional<Integer> firstEven = numbers.stream().filter(number -> number % 2 == 0) // Filter even numbers
+                                                     .findFirst(); // Get the First Even Number
 
-        // Print the filtered list
-        System.out.println("Even Numbers: " + evenNumbers);
+        // Print the first even number if present
+        firstEven.ifPresent(number -> System.out.println("First Even Number: " + number));
 
     }
 }
