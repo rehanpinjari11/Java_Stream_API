3bcc336ba513e18f4fa2bb903dbe5cc18220b7d3
Rehan Pinjari <pinjarirehan11@gmail.com>
2024-10-03 18:11:47 +0530

    Added: Find Max and Min Even Number Using Max and Min Function

--- src/com/bridglabz/java/streamsapi/StreamExample.java
+++ src/com/bridglabz/java/streamsapi/StreamExample.java
@@ -1,22 +1,34 @@
 package com.bridglabz.java.streamsapi;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class StreamExample {
 
     public static void main(String[] args)
     {
         //Create a list of integers
-        List<Integer> numbers = Arrays.asList(1, 3, 5, 6, 7, 8, 10, 11, 12);
+        List<Integer> numbers = Arrays.asList(7, 3, 10, 4, 9, 6, 2, 15);
 
         // Use stream.filter() to find even numbers, and findFirst() to get the first even number
-        Optional<Integer> firstEven = numbers.stream().filter(number -> number % 2 == 0) // Filter even numbers
-                                                     .findFirst(); // Get the First Even Number
+        List<Integer> evenNumbers = numbers.stream().filter(number -> number % 2 == 0) // Filter even numbers
+                                                     .collect(Collectors.toList()); // Collect into List
 
-        // Print the first even number if present
-        firstEven.ifPresent(number -> System.out.println("First Even Number: " + number));
+        // Print the filtered even numbers
+        System.out.println("Even Numbers: " + numbers);
+
+        // Find the minimum and maximum even numbers
+        Optional<Integer> minEven = evenNumbers.stream().min(Comparator.naturalOrder()); // Find the minimum even Number
+
+        Optional<Integer> maxEven = evenNumbers.stream().max(Comparator.naturalOrder()); // Find the maximum even Number
+
+        // Print the minimum and maximum even numbers if present
+
+        minEven.ifPresent(min -> System.out.println("Minimum Even Number: " + min));
+        maxEven.ifPresent(max -> System.out.println("Maximum Even Number: " + max));
 
     }
 }
