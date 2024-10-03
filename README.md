333aa014e705b237760e32205e0e26bf7f45ad02
Rehan Pinjari <pinjarirehan11@gmail.com>
2024-10-03 16:42:45 +0530

    Added: Welcome Message is added

---
+++ README.md
@@ -0,0 +1,1 @@
+# Java_Stream_API
---
+++ src/com/bridglabz/java/streamsapi/StreamExample.java
@@ -0,0 +1,10 @@
+package com.bridglabz.java.streamsapi;
+
+public class StreamExample {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Java Streams API Program");
+
+    }
+}
