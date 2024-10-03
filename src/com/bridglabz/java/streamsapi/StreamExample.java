7f61fcbfe449f373d094543e22705620b641148e
Rehan Pinjari <pinjarirehan11@gmail.com>
2024-10-03 17:33:09 +0530

    Added: Filter The Even Numbers From Number Stream Using Filter Function

--- src/com/bridglabz/java/streamsapi/StreamExample.java
+++ src/com/bridglabz/java/streamsapi/StreamExample.java
@@ -9,14 +9,14 @@ public class StreamExample {
     public static void main(String[] args)
     {
         //Create a list of integers
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
         // Use stream.map() to transform each element to double and store the result
-        List<Double> doubleNumbers = numbers.stream().map(number -> number * 2.0) //Transform each element to double
-                                                     .collect(Collectors.toList()); // Collect the result into a new list
+        List<Integer> evenNumbers = numbers.stream().filter(number -> number % 2 == 0) // Filter even numbers
+                                                     .collect(Collectors.toList()); // Collect the filtered numbers into a new list
 
-        // Print the new list of doubles
-        System.out.println("Transformed List: " + doubleNumbers);
+        // Print the filtered list
+        System.out.println("Even Numbers: " + evenNumbers);
 
     }
 }
