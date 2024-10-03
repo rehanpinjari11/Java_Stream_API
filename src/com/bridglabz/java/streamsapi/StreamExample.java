49e77005877e80e586995bfe64ce69318f1036bc
Rehan Pinjari <pinjarirehan11@gmail.com>
2024-10-03 16:49:57 +0530

    Added: Iterating Elements Using forEach

--- src/com/bridglabz/java/streamsapi/StreamExample.java
+++ src/com/bridglabz/java/streamsapi/StreamExample.java
@@ -1,10 +1,18 @@
 package com.bridglabz.java.streamsapi;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StreamExample {
 
     public static void main(String[] args)
     {
-        System.out.println("Welcome to Java Streams API Program");
+        //Create a list of integers
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        // Create a stream from the list and use forEach to iterate over the elements
+        numbers.stream().forEach(number -> System.out.println("Number " + number));
+
 
     }
 }
