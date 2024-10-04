be9f68f63d705a6f4c067c5f4b7bd19d22fa8a16
Rehan Pinjari <pinjarirehan11@gmail.com>
2024-10-04 10:04:57 +0530

    Added: Find the Sum And Average Of The Number Stream

--- src/com/bridglabz/java/streamsapi/StreamExample.java
+++ src/com/bridglabz/java/streamsapi/StreamExample.java
@@ -1,10 +1,8 @@
 package com.bridglabz.java.streamsapi;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.OptionalDouble;
 
 public class StreamExample {
 
@@ -13,22 +11,18 @@ public class StreamExample {
         //Create a list of integers
         List<Integer> numbers = Arrays.asList(7, 3, 10, 4, 9, 6, 2, 15);
 
-        // Use stream.filter() to find even numbers, and findFirst() to get the first even number
-        List<Integer> evenNumbers = numbers.stream().filter(number -> number % 2 == 0) // Filter even numbers
-                                                     .collect(Collectors.toList()); // Collect into List
+        // Find the sum of the numbers in the stream
+        int sum = numbers.stream().mapToInt(Integer::intValue)  // Convert Integer to int
+                                  .sum();  // Find the sum of the numbers
 
-        // Print the filtered even numbers
-        System.out.println("Even Numbers: " + numbers);
 
-        // Find the minimum and maximum even numbers
-        Optional<Integer> minEven = evenNumbers.stream().min(Comparator.naturalOrder()); // Find the minimum even Number
+        // Find the average of the numbers in the stream
+        OptionalDouble average = numbers.stream().mapToInt(Integer::intValue)  // Convert Integer to int
+                                                 .average();  // Find the average of the numbers
 
-        Optional<Integer> maxEven = evenNumbers.stream().max(Comparator.naturalOrder()); // Find the maximum even Number
-
-        // Print the minimum and maximum even numbers if present
-
-        minEven.ifPresent(min -> System.out.println("Minimum Even Number: " + min));
-        maxEven.ifPresent(max -> System.out.println("Maximum Even Number: " + max));
+        // Print the sum and average
+        System.out.println("Sum: " + sum);
+        average.ifPresent(avg -> System.out.println("Average: " + avg));
 
     }
 }
